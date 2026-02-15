817c0111530fc5619a97ae5764b73617a6553a63
maximumg9 <maximumg999@gmail.com>
2026-02-15 06:55:26 +0000

    Change tweakeroo version, and get the tweakeroo version in a better way

--- src/main/java/com/maximumg9/g9utils/mixins/mixmixin/ServerDataSyncerMixin.java
+++ src/main/java/com/maximumg9/g9utils/mixins/mixmixin/ServerDataSyncerMixin.java
@@ -2,12 +2,12 @@ package com.maximumg9.g9utils.mixins.mixmixin;
 
 import com.maximumg9.g9utils.G9utils;
 import fi.dy.masa.tweakeroo.data.DataManager;
-import fi.dy.masa.tweakeroo.data.ServerDataSyncer;
+import fi.dy.masa.tweakeroo.data.EntityDataManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ServerDataSyncer.class)
+@Mixin(EntityDataManager.class)
 public class ServerDataSyncerMixin {
     @Redirect(
         method = "onWorldPre",
